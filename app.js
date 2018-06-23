dbe12abfd8cd3359e48adaccb5c3afd7a81d3616
Yuki Nozaki <nq127main@gmail.com>
2018-06-23 18:56:49 +0900

    init commit

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const router = express.Router();
+console.log("kore de iino ?");
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,7 @@
+import * as express from 'express';
+
+const router: express.Router = express.Router();
+
+router.get('/', (req: express.Request, res: express.Response) => {
+    
+})
